f2579d8f2becacbdac9f03ae1b15402e66a56148
Colleen K. Brand <ckbrand@umich.edu>
2017-03-01 09:28:54 -0500

    Process data thorugh mothur MiSeq SOP

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -11,7 +11,7 @@ rm StabilityNoMetaG.tar
 
 curl -LO http://mothur.org/w/images/1/15/Silva.seed_v123.tgz
 tar xvzf Silva.seed_v123.tgz silva.seed_v123.align silva.seed_v123.tax
-mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
+code/mothur/mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
 mv silva.seed_v123.pick.align data/references/silva.seed.align
 rm Silva.seed_v123.tgz silva.seed_v123.*
 rm mothur.*.logfile
@@ -24,3 +24,17 @@ tar xvzf Trainset14_032015.pds.tgz trainset14_032015.pds/trainset14_032015.pds.*
 mv trainset14_032015.pds/* data/references/
 rmdir trainset14_032015.pds
 rm Trainset14_032015.pds.tgz
+
+# Generate a customized version of the SILVA reference database that targets the V4 region
+code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
+mv data/references/silva.seed.pcr.align data/references/silva.v4.align
+
+
+#Process raw sequences to the point where they are fully curated
+code/mothur/mothur code/get_good_seqs.batch
+
+#Calculate the sequencing error rate
+code/mothur/mothur code/get_error.batch
+
+#Generate the shared files
+code/mothur/mothur code/get_shared_otus.batch
